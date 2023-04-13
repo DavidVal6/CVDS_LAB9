027786a39ea31ad5a7e53212213542790b8bc9e6
DavidVal6 <98176834+DavidVal6@users.noreply.github.com>
2023-04-13 00:32:48 -0500

    Logica  #250

---
+++ servlet/src/main/java/edu/eci/cvds/servlet/Login/UsuarioBean.java
@@ -0,0 +1,55 @@
+package edu.eci.cvds.servlet.Login;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import java.io.Serializable;
+
+@Component
+@ManagedBean(name = "usuarioBean")
+@SessionScoped
+public class UsuarioBean implements Serializable {
+    private final UsuarioRepository usuarioRepository;
+    @Autowired
+    UsuarioService usuarioService;
+    private String user;
+    private String password;
+
+    public UsuarioBean(UsuarioRepository usuarioRepository){
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    public String getUser(){
+        return user;
+    }
+
+    public void setUser(String user){
+        this.user = user;
+    }
+    public String getPassword(){
+        return password;
+    }
+    public String login (){
+        Usuario usuario = usuarioRepository.findById(user);
+        if(usuario == null || !usuario.getPassword().equals(password)){
+            FacesContext.getCurrentInstance().addMessage("@all", new FacesMessage(FacesMessage.SEVERITY_ERROR,"Los Datos No Coinciden",null));
+            return null;
+        }else{
+            return "welcome.xhtml";
+        }
+    }
+    @Bean
+    public CommandLineRunner usuarioActual() throws Exception{
+        return args -> {
+            usuarioService.addUsuario(new Usuario("admin","xdsrewq"));
+            usuarioService.addUsuario(new Usuario("david","123"));
+            usuarioService.getAllUsuario().forEach(System.out::println);
+        };
+    }
+}
